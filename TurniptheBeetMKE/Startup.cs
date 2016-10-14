89f134ed0c8ea765b4a66111458592de6824c55c
Sheng Cruz <vansx001@gmail.com>
2016-10-14 11:31:09 -0500

    initial commit

---
+++ TurniptheBeetMKE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TurniptheBeetMKE.Startup))]
+namespace TurniptheBeetMKE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
